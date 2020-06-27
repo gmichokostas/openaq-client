ad8c792d8518fb26effed10650befc8ddb251393
Yiorgos Michokostas <michokostas@gmail.com>
2020-06-27 22:12:54 +0200

    Version bump

--- lib/openaq/version.rb
+++ lib/openaq/version.rb
@@ -1,3 +1,3 @@
 module Openaq
-  VERSION = "0.2.0".freeze
+  VERSION = "0.3.0".freeze
 end
--- spec/openaq_spec.rb
+++ spec/openaq_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe Openaq do
   it "has a version number" do
     expect(Openaq::VERSION).not_to be nil
-    expect(Openaq::VERSION).to eql "0.2.0"
+    expect(Openaq::VERSION).to eql "0.3.0"
   end
 
   describe ".client" do
